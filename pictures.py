32d4003db934449f97cd4c8c1006c9e9c441e9ee
Eduardo Ramírez <ejramire@uc.cl>
2013-10-30 16:06:14 -0300

    corrected path

--- pictures.py
+++ pictures.py
@@ -13,11 +13,10 @@ for serie in series.find({},{'pic': True, '_id':False}):
 	urls.append(actor['pic'])
 for url in urls:
 	path = url.split('/')[3:]
-	print path
 	pathname = "../"
 	for i in path[:-1]:
 		#if folder doesn't exist create
-		pathname += i
+		pathname += i + '/'
 		if not os.path.exists(pathname):
 			os.makedirs(pathname)
 	#if picture (path[-1]) doesn't exist, download 
