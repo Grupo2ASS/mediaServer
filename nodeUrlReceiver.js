9f03837e9b07ab8b9e368c1a1a57eb9acc5eb41c
ejramire <ejramire@uc.cl>
2013-11-14 02:23:44 -0300

    package.json

--- nodeUrlReceiver.js
+++ nodeUrlReceiver.js
@@ -3,14 +3,14 @@
   var url = require('url');
   var sys = require('sys');
   var exec = require('child_process').exec;
+  var _ = require('underscore');
   var child;
   
-  http.createServer(function (req, res) {
+  http.createServer(function (req, res ) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var url_parts = url.parse(req.url, true);
-    console.log(url_parts);
     console.log(url_parts.query);
-    try{
+    //try{
     var image_url = url_parts.query['image_url'];
 /*What was on original script
  path = url.split('/')[3:]
@@ -29,26 +29,30 @@
 			print "Added " + additions.to_s + " new pictures"
 */
     var path = image_url.split('/').splice(3);
-    var pathname = "../"
-    _.each(path.slice(0,path.len-1),function(elem){
+    var pathname = "../";
+    _.each(path,function(elem, index){
 	pathname += elem + '/';
-	if(!fs.existsSync(pathname)){
+	if(!(fs.existsSync(pathname))){
+		if(index == path.length-1){
+			child = exec('curl -o '+pathname+path[path.length-1]+' '+image_url, function (error, stdout, stderr) {
+				sys.print('stdout: ' + stdout);
+				sys.print('stderr: ' + stderr);
+				console.log("downloaded file " + pathname+path[path.length-1]);
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+   			 });
+		}
+		else{
 		fs.mkdirSync(pathname);
+	}
 	}
 
     });
-    child = exec('curl -o '+pathname+path[path.length-1]+' '+image_url, function (error, stdout, stderr) {
-	sys.print('stdout: ' + stdout);
-	sys.print('stderr: ' + stderr);
-	if (error !== null) {
-		console.log('exec error: ' + error);
-	}
-    });
-    }
-   catch(e){
-	 console.log(e);
-   } 
-    console.log(path);
+  // catch(e){
+   // }
+	 //console.log(e);
+   //} 
     
     res.end("holi");
   }).listen(3000);
