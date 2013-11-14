441d236b8c38c4db8b9831596811f6c037285982
Eduardo Ramírez <ejramire@uc.cl>
2013-11-14 11:26:05 -0300

    fixed '/' issue

--- nodeUrlReceiver.js
+++ nodeUrlReceiver.js
@@ -4,34 +4,20 @@
   var sys = require('sys');
   var exec = require('child_process').exec;
   var _ = require('underscore');
+  var mkdirp = require('mkdirp');
   var child;
   
   http.createServer(function (req, res ) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var url_parts = url.parse(req.url, true);
     console.log(url_parts.query);
-    //try{
+    try{
     var image_url = url_parts.query['image_url'];
-/*What was on original script
- path = url.split('/')[3:]
-			pathname = "../"
-			for i in path[:-1]:
-				#if folder doesn't exist create
-				pathname += i + '/'
-				if not os.path.exists(pathname):
-					os.makedirs(pathname)
-			#if picture (path[-1]) doesn't exist, download 
-				#curl url to path
-			if not os.path.exists(pathname + path[-1]):
-				os.system('curl -o '+pathname+path[-1]+' '+url)
-				additions += 1
-		if additions:
-			print "Added " + additions.to_s + " new pictures"
-*/
     var path = image_url.split('/').splice(3);
     var pathname = "../";
     _.each(path,function(elem, index){
-	pathname += elem + '/';
+	index != path.length-1 ? pathname += elem + '/': pathname += elem;
+	
 	if(!(fs.existsSync(pathname))){
 		if(index == path.length-1){
 			child = exec('curl -o '+pathname+path[path.length-1]+' '+image_url, function (error, stdout, stderr) {
@@ -49,10 +35,10 @@
 	}
 
     });
-  // catch(e){
-   // }
-	 //console.log(e);
-   //} 
+    }
+   catch(e){
+	 console.log(e);
+   } 
     
     res.end("holi");
   }).listen(3000);
